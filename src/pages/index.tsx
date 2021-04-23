3197f7e70fdeec59b96ddd98bb2e50125dcf5083
ydrouulis <lombardi.droulis@gmail.com>
2021-04-23 09:54:49 -0300

    import context

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import { api } from "../services/api"
 import { format, parseISO } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 import { convertDurationToTimeString } from "../utils/convertDurationToTimeString"
+import { useContext } from "react"
+import { PlayerContext } from "../contexts/playerContext"
 
 import styles from './home.module.scss'
 
@@ -35,6 +37,8 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
   //   .then(data => console.log(data))
   // }, [])
 
+  const { play } = useContext(PlayerContext);
+
   return (
     <div className={styles.homePage}>
       <section className={styles.latestEpisodes}>
@@ -53,7 +57,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type='button'>
+                <button type='button' onClick={() => play(episode)}>
                   <img src="/play-green.svg" alt="Tocar episódio" />
                 </button>
               </li>
