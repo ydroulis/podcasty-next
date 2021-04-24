830986c75a3bd6b47aa710595cb6fcfb117a9ccf
ydrouulis <lombardi.droulis@gmail.com>
2021-04-24 11:28:55 -0300

    make logo in a link to return home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import format from 'date-fns/format'
 import ptBR from 'date-fns/locale/pt-BR'
+import Link from 'next/link';
 
 import styles from './styles.module.scss';
 
@@ -10,7 +11,9 @@ export default function Header() {
 
     return(
         <header className={styles.headerContainer}>
-            <img src="/logo.svg" alt="Podcasty logo"/>
+            <Link href='/'>
+                <img src="/logo.svg" alt="Podcasty logo"/>
+            </Link>
             <p>O melhor para você ouvir, sempre</p>
             <span>{currentDate}</span>
         </header>
